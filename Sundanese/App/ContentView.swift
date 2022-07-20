f91cb1549a7cadf229fbdb2e0720a02ecffe8609
rahmanarif47 <rahmanarif.9764@gmail.com>
2022-07-20 23:50:42 +0700

    on develop

--- Sundanese/App/ContentView.swift
+++ Sundanese/App/ContentView.swift
@@ -10,12 +10,14 @@ import CoreData
 
 struct ContentView: View {
     var body: some View {
-        
+        Text("Content")
+            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        
+        ContentView()
+            .previewDevice("iPhone 8")
     }
 }
--- Sundanese/App/SundaneseApp.swift
+++ Sundanese/App/SundaneseApp.swift
@@ -13,7 +13,7 @@ struct SundaneseApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
