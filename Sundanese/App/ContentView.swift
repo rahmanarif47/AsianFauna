40e2d4c5cdbffc22230cd1fd82a216f86fc5fc8e
rahmanarif47 <rahmanarif.9764@gmail.com>
2022-07-20 23:47:57 +0700

    first commit

---
+++ Sundanese/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sundanese
+//
+//  Created by Arif Rahman Sidik on 20/07/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+    }
+}
